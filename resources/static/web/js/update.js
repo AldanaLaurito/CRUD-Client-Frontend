9bebe1a426ba153356b68f0a4bed6669e002d2f3
Tobias Moreno <tobias8012@hotmail.com>
2018-09-03 11:23:23 -0300

    Auto stash before merge of "combinacion" and "CreateClient"

--- resources/static/web/js/update.js
+++ resources/static/web/js/update.js
@@ -1,26 +1,26 @@
 // Get the modal
-var modal = document.getElementById('updateModal');
+var updateModal = document.getElementById('updateModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("updateBtn");
+var updateBtn = document.getElementById("updateBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("updateClose")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+updateBtn.onclick = function() {
+    updateModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    updateModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == updateModal) {
+        updateModal.style.display = "none";
     }
 }
 
