7b6a8e148fc913a94ba26bcdeb845141a482cd29
Aldana Laurito <aldana.laurito.AL@gmail.com>
2018-09-03 10:13:57 -0300

    some changes

--- resources/static/web/js/create-client.js
+++ resources/static/web/js/create-client.js
@@ -2,7 +2,8 @@
     document.getElementById("create-client-form").submit();
 }*/
 $(document).ready(function(){
-    $("create-client-form").submit(function(){
+    $("create-client-form").submit(function(event){
         alert("Submitted");
+        event.preventDefault();
     });
 });
