0dacffb851917551eaa4ed8a4970c81e6d160780
Aldana laurito <c5102076@nwytg.net>
2018-09-04 13:52:04 -0300

    javascript change

--- resources/static/web/js/table.js
+++ resources/static/web/js/table.js
@@ -53,6 +53,32 @@ var app = new function() {
         }
     };
     this.Edit = function (item) {
+        var updateModal = document.getElementById('updateModal');
+
+// Get the button that opens the modal
+        var updateBtn = document.getElementById("updateBtn");
+
+// Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("updateClose")[0];
+
+// When the user clicks on the button, open the modal
+        updateBtn.onclick = function() {
+            updateModal.style.display = "block";
+        }
+
+// When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            updateModal.style.display = "none";
+        }
+
+// When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == updateModal) {
+                updateModal.style.display = "none";
+            }
+        }
+
+        this.el = document.getElementById('');
         var el = document.getElementById('formGroupExampleInput');
         var el1 = document.getElementById('formGroupExampleInput2');
         // Display value in the field
